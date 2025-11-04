ed19623f42fe2fcd592136c080dd2bd09d458377
Avay kumar shaw <111166830+avay0809@users.noreply.github.com>
2025-11-04 18:13:12 +0000

    operation

--- 01_Basics/03_conversion&Operation.js
+++ 01_Basics/03_conversion&Operation.js
@@ -1,8 +1,27 @@
-let score = 33
-let score = "33"
+//let score = 33
+// let score = "33abc"
+// let score = null
+let score = "Avay"
 
 
-console.log(typeof score);
-console.log(typeof (score));
+
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "Avay"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false       empety string ka liya false ayega
+//"Avay" => true     kisi name ka liya true value dega
